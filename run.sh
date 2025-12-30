e34b80de2ddee559f28cdff0ec5d4d7e17f22a11
Wang Sen Di <wangsendi@vip.qq.com>
2025-12-30 14:29:11 +0800

    chore 2025-12-30 14:29:11 CST

--- run.sh
+++ run.sh
@@ -2,22 +2,28 @@
 
 set -euo pipefail
 
-# 检查并安装 stow
-command -v stow >/dev/null 2>&1 || apt install -y stow
+__main() {
+	# 检查并安装 stow
+	command -v stow >/dev/null 2>&1 || apt install -y stow
 
-# 获取 dotfiles 目录
-_dotfiles_dir="${DOTFILES_DIR:-$HOME/.dotfiles}"
+	# 获取 dotfiles 目录
+	_dotfiles_dir="${DOTFILES_DIR:-$HOME/.dotfiles}"
 
-# 如果不存在就 clone
-[[ -d "$_dotfiles_dir" ]] || git clone https://github.com/wangsendi/dotfiles.git "$_dotfiles_dir"
+	# 如果不存在就 clone
+	if [[ ! -d "$_dotfiles_dir" ]]; then
+		git clone https://github.com/wangsendi/dotfiles.git "$_dotfiles_dir"
+	else
+		git -C "$_dotfiles_dir" pull
+	fi
 
-cd "$_dotfiles_dir"
+	cd "$_dotfiles_dir"
 
-# 执行 stow
-for _dir in */; do
-	[[ -d "$_dir" ]] && [[ ! "$_dir" =~ ^\.git ]] || continue
-	echo "Stowing $_dir..."
-	stow -t "$HOME" -d "$_dotfiles_dir" "$_dir" --ignore="\.git"
-done
+	# 执行 stow
+	for _dir in */; do
+		[[ -d "$_dir" ]] && [[ ! "$_dir" =~ ^\.git ]] || continue
+		echo "Stowing $_dir..."
+		stow -t "$HOME" -d "$_dotfiles_dir" "$_dir" --ignore="\.git"
+	done
 
-echo "Done!"
+	echo "Done!"
+}
